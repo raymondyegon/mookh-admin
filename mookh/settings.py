c85f172e608f14c3056e305b4ba91d86493e8568
jameskomo <james.komoh@gmail.com>
2019-06-13 18:06:30 +0300

    Dramatiq changes migrated and tasks creation initiated

--- mookh/settings.py
+++ mookh/settings.py
@@ -21,8 +21,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 't5q9c&24jwr!dq66yv6%te%_qt)vkifylw$&q^az6-s&i-tasy'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-# DRAMATIQ BROKER SETTINGS
+ALLOWED_HOSTS = []
+
+# Twilio API
+TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+
 DRAMATIQ_BROKER = {
     "BROKER": "dramatiq.brokers.redis.RedisBroker",
     "OPTIONS": {
@@ -39,52 +47,52 @@ DRAMATIQ_BROKER = {
     ]
 }
 
-# Defines which database should be used to persist Task objects when the
-# AdminMiddleware is enabled.  The default value is "default".
-# DRAMATIQ_TASKS_DATABASE = "default"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Reminder time: how early text messages are sent in advance of schedules
+REMINDER_TIME = 30  # minutes
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
-INSTALLED_APPS = [
-    'bootstrap4',
-    'django_forms_bootstrap',
-    'rest_framework.authtoken',
-    'rest_framework',
-    'django-dramatiq',
-    'timezone_field',
-    'imagekit',
-    'django_registration',
-    'automation',
+DJANGO_APPS = (
+    'django_dramatiq',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+    'django.contrib.staticfiles'
+)
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+THIRD_PARTY_APPS = (
+    'bootstrap4',
+    'django_forms_bootstrap',
+    'timezone_field'
+)
+
+LOCAL_APPS = (
+    'automation',
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 ROOT_URLCONF = 'mookh.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -97,11 +105,13 @@ TEMPLATES = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 WSGI_APPLICATION = 'mookh.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -113,31 +123,12 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -147,6 +138,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = BASE_DIR + '/staticfiles'
 
 STATIC_URL = '/static/'
