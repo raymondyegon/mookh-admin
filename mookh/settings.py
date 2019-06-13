a109023c3ad19760b2cadc0789c8a63ee926e4b7
jameskomo <james.komoh@gmail.com>
2019-06-13 10:27:24 +0300

    Scheduling model installed and django-timezone dependency installed

--- mookh/settings.py
+++ mookh/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'mookh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mookh_project',
+        'USER': 'komo',
+        'PASSWORD': 'komo',
     }
 }
 
