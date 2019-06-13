c6901cfc45f18b34f1bb7308bbb0a4016f55e0de
Raymond Yegon <raymondyegon17@gmail.com>
2019-06-13 07:50:46 +0300

    Formed the basic structure of the web application

--- mookh/settings.py
+++ mookh/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'rest_framework.authtoken',
+    'rest_framework',
+    'imagekit',
+    'django_registration',
+    'automation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
