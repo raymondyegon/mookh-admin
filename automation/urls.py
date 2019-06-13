8e472594b4cb7ddb6ee2f383fd9419643904ba5e
jameskomo <james.komoh@gmail.com>
2019-06-13 14:02:10 +0300

    List Views created for lopping through schedules

--- automation/urls.py
+++ automation/urls.py
@@ -3,12 +3,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import re_path
-from .views import SchedulingCreateView
+from .views import SchedulingCreateView, SchedulingListView, SchedulingDeleteView, SchedulingUpdateView, SchedulingDetailView
 
 
 
 urlpatterns = [
-    re_path(r'^new$', SchedulingCreateView.as_view(), name='new_schedule'),
+    path('new/', SchedulingCreateView.as_view(), name='new_schedule'),
+    # re_path(r'^new$', SchedulingCreateView.as_view(), name='new_schedule'),
+    re_path(r'^$', SchedulingListView.as_view(), name='list_schedules'),
 ]
 
 
--- automation/views.py
+++ automation/views.py
@@ -3,11 +3,45 @@ from .models import Scheduling
 from django.views.generic import CreateView
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import DeleteView
+from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
 
 # Create your views here.
+class SchedulingListView(ListView):
+    """Shows users a list of schedules"""
+
+    model = Scheduling
+
+
+class SchedulingDetailView(DetailView):
+    """Shows users a single schedule"""
+
+    model = Scheduling
+
+
 class SchedulingCreateView(SuccessMessageMixin, CreateView):
-    """Powers a form to create a new appointment"""
+    """Powers a form to create a new schedule"""
 
     model = Scheduling
     fields = ['name', 'phone_number', 'time', 'time_zone']
-    success_message = 'Appointment successfully created.'
+    success_message = 'Schedule successfully created.'
+
+
+class SchedulingUpdateView(SuccessMessageMixin, UpdateView):
+    """Powers a form to edit existing schedules"""
+
+    model = Scheduling
+    fields = ['name', 'phone_number', 'time', 'time_zone']
+    success_message = 'Schedule successfully updated.'
+
+
+class SchedulingDeleteView(DeleteView):
+    """Prompts users to confirm deletion of an schedule"""
+
+    model = Scheduling
+    success_url = reverse_lazy('list_schedules')
