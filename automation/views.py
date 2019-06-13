1032fcc3f4df3aef6818e5ad6b266e229c236dfa
jameskomo <james.komoh@gmail.com>
2019-06-13 10:31:23 +0300

    Scheduling view created with class based views and linked to Scheduling model. SuccessMessage Mixing applied as well

--- automation/views.py
+++ automation/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Scheduling
 
 # Create your views here.
+class SchedulingCreateView(SuccessMessageMixin, CreateView):
+    """Powers a form to create a new appointment"""
+
+    model = Scheduling
+    fields = ['name', 'phone_number', 'time', 'time_zone']
+    success_message = 'Appointment successfully created.'
