c528a36e753c90acf2be38d89449703c6ab695a3
willy_kid <nillyw31@gmail.com>
2019-06-17 11:35:05 +0300

    login functionality done

--- languages/views.py
+++ languages/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Language
 from .serializers import LanguageSerializer, UserSerializer
 
 from django.contrib.auth.models import User
 
+
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
