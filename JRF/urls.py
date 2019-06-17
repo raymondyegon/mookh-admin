fa3a7be578ac24db6744f07a5287bb70f40df721
willy_kid <nillyw31@gmail.com>
2019-06-17 11:56:05 +0300

    static files

--- JRF/urls.py
+++ JRF/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('languages.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api.token/refresh', TokenRefreshView.as_view())
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('languages.urls')),
+                  path('api-auth/', include('rest_framework.urls')),
+                  path('api/token/', TokenObtainPairView.as_view()),
+                  path('api.token/refresh', TokenRefreshView.as_view())
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
