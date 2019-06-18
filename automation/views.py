f52354aa7b3935bfa815dcead6629bbf225ae757
jameskomo <james.komoh@gmail.com>
2019-06-18 13:07:00 +0300

    Email Alternatives implemented in Sendgrid email feature

--- automation/views.py
+++ automation/views.py
@@ -52,31 +52,24 @@ from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 
 def sendEmail(request):
-    send_mail("Your Subject", "This is a simple text email body.",
-    "James Komo <james.komoh@gmail.com>", ["juniorgichohi@gmail.com"])
     mail = EmailMultiAlternatives(
-    subject="Your Subject",
+    subject="Email Test",
     body="This is a simple text email body.",
-    from_email="James Komo <james.komoh@gmail.com>",
+    from_email="K.O.M.O <james.komoh@gmail.com>",
     to=["juniorgichohi@gmail.com"],
+    headers={"Reply-To": "juniorgichohi@gmail.com"}
     )
     # Add template
-    mail.template_id = 'd-dee240da903344fdb5c3f16896f489b0'
+    # mail.template_id = 'YOUR TEMPLATE ID FROM SENDGRID ADMIN'
 
-    # # Replace substitutions in sendgrid template
-    # mail.substitutions = {'%username%': 'komo'}
+    # Replace substitutions in sendgrid template
+    mail.substitutions = {'%username%': 'komo'}
 
     # Attach file
-    # with open('somefilename.pdf', 'rb') as file:
-    #     mail.attachments = [
-    #         ('somefilename.pdf', file.read(), 'application/pdf')
-    #     ]
-
-    # Add categories
-    mail.categories = [
-        'work',
-        'urgent',
-    ]
+    with open('emailTest.pdf', 'rb') as file:
+        mail.attachments = [
+            ('emailTest.pdf', file.read(), 'application/pdf')
+        ]
 
     mail.attach_alternative(
         "<p>This is a simple HTML email body</p>", "text/html"
